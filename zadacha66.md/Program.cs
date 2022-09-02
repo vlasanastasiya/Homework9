c1ce94aa9d9e985e1b3734cbed2f158776b72b80
vlasenkova <asyicheva1@mail.ru>
2022-09-02 13:36:45 +0300

    second  ex

---
+++ zadacha66.md/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int numberM = 4;
+int numberN = 8;
+void Findsum(int numberM, int numberN, int sum = 0)
+{ 
+     
+    if (numberM > numberN)
+    {
+    Console.WriteLine(sum);
+    return;
+    }
+    sum = sum + numberM; 
+    numberM++;
+   
+    Findsum(numberM,numberN, sum);
+}
+Findsum(numberM, numberN);
